9635e95585a887c77f23f9caa48c7d2483b5b847
Tristry <149309903+Tristry@users.noreply.github.com>
2023-10-30 23:25:32 +0530

    asdasd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,50 +12,50 @@
 			name: 'Risk Assessment',
 			description:
 				'We encourage kids to make their own risk assessments with minimal adult interference which allows children the space and time to develop natural problem solving skills. We foster a sense of responsibility and judgement that can benefit them in various aspects of life.',
-			image: 'https://cdn.midjourney.com/3acad77c-0d08-41c0-be4c-df59ee102322/0_1.webp'
+			image: 'https://res.cloudinary.com/dk8kuqbbc/image/upload/f_auto/q_auto/v1698686600/Risk%20Assessment.webp'
 		},
 		{
 			name: 'Curiosity and Learning',
 			description:
 				'By nurturing curiosity, our clubs help children become curious learners for life, promoting a thirst for knowledge that goes beyond the classroom.',
-			image: 'https://cdn.midjourney.com/1334ccf7-2026-49de-b0ec-57612c47a4de/0_1.webp'
+			image: 'https://res.cloudinary.com/dk8kuqbbc/image/upload/f_auto/q_auto/v1698686022/Curiosity%20and%20Learning.webp'
 		},
 		{
 			name: 'Physical Development',
 			description:
 				'Our clubs support the development of stronger physical abilities in children through outdoor activities, promoting health and well-being.',
-			image: 'https://cdn.midjourney.com/1259a882-c70c-444d-91e0-413fac57073e/0_3.webp'
+			image: 'https://res.cloudinary.com/dk8kuqbbc/image/upload/f_auto/q_auto/v1698686350/Physical%20Development.webp'
 		},
 		{
 			name: 'Emotional Resilience',
 			description:
 				'The outdoor setting allows children to develop emotional resilience by navigating the challenges and uncertainties of the natural world, helping them build inner strength.',
-			image: 'https://cdn.midjourney.com/eac06a5c-62f8-446f-845a-6556c820ede0/0_0.webp'
+			image: 'https://res.cloudinary.com/dk8kuqbbc/image/upload/f_auto/q_auto/v1698686091/Emotional%20Resilience.webp'
 		},
 
 		{
 			name: 'Effective Communication',
 			description:
 				'Children attending our clubs become more effective communicators as they learn to express themselves in a diverse and interactive outdoor environment, improving their social skills.',
-			image: 'https://cdn.midjourney.com/dd1f6e4f-e47e-4088-88f5-27fa7c272aa5/0_2.webp'
+			image: 'https://res.cloudinary.com/dk8kuqbbc/image/upload/f_auto/q_auto/v1698686061/Effective%20Communication.webp'
 		},
 		{
 			name: 'Self-Confidence',
 			description:
 				'Participation in our clubs can boost a childs self-confidence, as they conquer outdoor challenges and gain a sense of accomplishment.',
-			image: 'https://cdn.midjourney.com/cb9d7681-96a9-42d9-9543-a6b340313f53/0_1.webp'
+			image: 'https://res.cloudinary.com/dk8kuqbbc/image/upload/f_auto/q_auto/v1698686604/Self%20Confidence.webp'
 		},
 		{
 			name: 'Sensory Learning',
 			description:
 				'Our approach and clubs engage childrens senses - research has proven that information acquired through the use of all our senses leads to knowledge being ingrained rather than simply retained for a short period of time.',
-			image: 'https://cdn.midjourney.com/a4b45a20-90fe-464f-9b5c-df39ece1faf6/0_3.webp'
+			image: 'https://res.cloudinary.com/dk8kuqbbc/image/upload/f_auto/q_auto/v1698686605/Sensory%20Learning.webp'
 		},
 		{
 			name: 'Learning Through Play',
 			description:
 				'Our approach emphasises learning through play, which helps children engage with their environment in a fun and meaningful way, promoting a positive attitude toward learning in all stages of life',
-			image: 'https://cdn.midjourney.com/50793d7b-576d-4cb6-8f68-7b75f74c9c92/0_3.webp'
+			image: 'https://res.cloudinary.com/dk8kuqbbc/image/upload/f_auto/q_auto/v1698686176/Learning%20Through%20Play.webp'
 		}
 	];
 	let isSubmitting = false;
@@ -65,8 +65,9 @@
 <section class="h-[80vh] relative overflow-hidden z-0">
 	<div class="w-full h-full overflow-hidden">
 		<img
-			src="https://cdn.discordapp.com/attachments/1020025379840327741/1159805988778541156/anarkyyy_bright_sun_shines_through_a_woods._in_the_style_of_nat_44860b68-629b-4417-ac5a-2d12d3904c10.png?ex=653b96ff&is=652921ff&hm=feb2cdc56013d94f75a02d7878fbd8ae4deaf3e3b90b027c0edd93d619cf939d&"
+			src="https://res.cloudinary.com/dk8kuqbbc/image/upload/f_auto/q_auto/v1698688020/Home%20page%20banner.webp"
 			alt=""
+			loading="lazy"
 			class="w-full h-full object-cover"
 		/>
 		<div class="absolute top-0 z-10 bg-black/50 h-[100vh] w-full transition-all" />
